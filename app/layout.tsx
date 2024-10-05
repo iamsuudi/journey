bed95b08acaf9b34624042ec08d59dbc92036064
Abdulfetah <suudiabdulfetah@gmail.com>
2024-10-05 15:12:09 +0300

    feat: Add sparkles for background

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 import NavBar from "@/app/_components/navbar";
+import { SparklesCore } from "@/components/ui/sparkles";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -21,10 +22,15 @@ export default function RootLayout({
         <html lang="en">
             <body
                 className={cn(
-                    "min-h-screen font-sans antialiased bg-black",
+                    "font-sans antialiased bg-black relative",
                     inter.variable
                 )}
             >
+                <SparklesCore
+				className="w-full h-full -z-[100] absolute"
+				background="#00000000"
+				particleDensity={10}
+			 />
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="dark"
